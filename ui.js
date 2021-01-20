1b33b92f7c38a5b8080c2472d0bdea251aa52c32
Marco De Canal <marco.decanal@gmail.com>
2021-01-20 19:15:26 +0100

    Architecture: basic navigation

--- ui.js
+++ ui.js
@@ -1,10 +1,14 @@
+"use strict";
+const header = document.querySelector(".jumbotron");
+const pages = document.querySelectorAll(".page");
 const contentDivs = document.querySelectorAll(".content-div");
 const navBtn = document.querySelector(".navbar-toggler");
 const navbar = document.querySelector(".navbar-main");
-const navItems = document.querySelectorAll(".nav-item");
+const navLinks = document.querySelectorAll(".nav-link");
+const textLinks = document.querySelectorAll(".text-link");
 const brand = document.querySelector(".navbar-brand");
 const covidBtn = document.querySelector(".btn-covid-alert");
-let navMenuHeight = 40 * navItems.length;
+let navMenuHeight = 40 * navLinks.length;
 let navExpanded = false;
 
 // ----------- Intersection observer, for content-div animations -----------------
@@ -17,7 +21,7 @@ const showContentDiv = function (entries, obs) {
   entries.forEach((e) => {
     if (!e.isIntersecting) return;
     e.target.classList.remove("content-div--hidden");
-    obs.unobserve(e.target);
+    //obs.unobserve(e.target);
   });
 };
 const obs = new IntersectionObserver(showContentDiv, obsOpt);
@@ -47,11 +51,6 @@ navBtn.addEventListener("click", () => {
   navExpanded = !navExpanded;
 });
 
-brand.addEventListener("click", (e) => {
-  e.preventDefault();
-  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-});
-
 // ------ Remove covid alert on user's confirm -----------------
 covidBtn.addEventListener("click", function (e) {
   // e.target.closest(".covid-alert").style.display = "none";
@@ -63,4 +62,57 @@ covidBtn.addEventListener("click", function (e) {
     () => e.target.closest(".covid-alert").classList.add("hidden"),
     1000
   );
+});
+
+// --------- navbar navigation --------------
+
+const navigateTo = function (
+  page,
+  top = header.getBoundingClientRect().height + 1
+) {
+  window.scrollTo({ behavior: "smooth", top: top });
+
+  // Show the correct page
+  pages.forEach((p) => {
+    if (p.classList.contains(page)) p.classList.remove("hidden");
+    else p.classList.add("hidden");
+  });
+  // Set active link
+  navLinks.forEach((l) => {
+    if (l.dataset.to === page) l.classList.add("active");
+    else l.classList.remove("active");
+  });
+};
+
+navbar.addEventListener("click", function (e) {
+  // Guard clause
+  if (!e.target.classList.contains("nav-link") && e.target !== brand) return;
+
+  e.preventDefault();
+
+  // Back to top of page
+  // const y = (header.getBoundingClientRect().height + 1).toFixed(0);
+  // window.scrollTo({ behavior: "smooth", top: y });
+
+  // Navigation
+  if (e.target.classList.contains("nav-link")) {
+    navigateTo(e.target.dataset.to);
+  }
+  if (e.target === brand) {
+    // Hide content divs, if not already on the home page
+    if (document.querySelector(".home-page").classList.contains("hidden"))
+      contentDivs.forEach((c) => {
+        c.classList.add("content-div--hidden");
+      });
+
+    navigateTo("home-page");
+  }
+});
+
+// ---------- navigation ----------------
+textLinks.forEach((l) => {
+  l.addEventListener("click", function (e) {
+    e.preventDefault();
+    navigateTo(e.target.dataset.to);
+  });
 });
