19ca17e3d7048d66bf4c7bdd496c39621bb1c268
Marco De Canal <marco.decanal@gmail.com>
2023-12-10 11:45:44 +0100

    footer tel link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -33,10 +33,10 @@ export default function Footer(props) {
         San Giovanni in Marignano, 47842, RN
       </h5>
       <h5 className={`mt-3 ${robotoLight.className}`}>
-        Marco De Canal <a href="tel:3385330241">338 533 0241</a>
+        <a href="tel:3385330241">Marco De Canal 338 533 0241</a>
       </h5>
       <h5 className={`${robotoLight.className}`}>
-        Giacomo Muccioli 339 318 2797
+        <a href="tel:3393182797">Giacomo Muccioli 339 318 2797</a>
       </h5>
     </section>
   );
